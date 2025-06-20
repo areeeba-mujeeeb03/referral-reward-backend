b952b7f91e526519e5fb7de718f19d7035c0d5b1
areeba-mujeeb03 <areeba.mujeeb@skillintelligence.tech>
2025-06-20 18:55:49 +0530

    RazikWorking

--- main_app/models/admin/galaxy_model.py
+++ main_app/models/admin/galaxy_model.py
@@ -1,11 +1,11 @@
-from mongoengine import StringField, Document, EmailField, DateTimeField, ListField, DictField
-import datetime
-
-class Galaxy(Document):
-    total_galaxies = StringField(required=True, unique=True)
-    all_galaxies = ListField(DictField)
-
-    meta = {'collection': 'galaxy_milestones'}
+# from mongoengine import StringField, Document, EmailField, DateTimeField, ListField, DictField
+# import datetime
+#
+# class Galaxy(Document):
+#     total_galaxies = StringField(required=True, unique=True)
+#     all_galaxies = ListField(DictField)
+#
+#     meta = {'collection': 'galaxy_milestones'}
 
 
 
